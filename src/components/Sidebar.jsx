e449f938358156978211bb79624ed4e9cb3dd35a
hasifjasmi <hasifj2207@gmail.com>
2023-01-28 18:29:07 +0800

    deelet

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -63,30 +63,6 @@ const Sidebar = () => {
               <br />
               <input type="submit" value="save" />
             </form>
-
-            {/* {links.map((item) => (
-              <div key={item.title}>
-                <p className="text-gray-400 dark:text-gray-400 m-3 mt-4 uppercase">
-                  {item.title}
-                </p>
-                {item.links.map((link) => (
-                  <NavLink
-                    to={`/${link.name}`}
-                    key={link.name}
-                    onClick={handleCloseSideBar}
-                    style={({ isActive }) => ({
-                      backgroundColor: isActive ? currentColor : "",
-                    })}
-                    className={({ isActive }) =>
-                      isActive ? activeLink : normalLink
-                    }
-                  >
-                    {link.icon}
-                    <span className="capitalize ">{link.name}</span>
-                  </NavLink>
-                ))}
-              </div>
-            ))} */}
           </div>
         </>
       )}
